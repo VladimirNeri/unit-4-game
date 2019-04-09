c603bbd10aa2c7f820d2cd1ee675e56da3b15b84
Vlad N <vlad.neri@gmail.com>
2019-04-08 20:30:15 -0700

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,61 +4,63 @@ var winCounter = 0;
 var lossCounter = 0;
 var counter = 0;
 
+    // Computer makes a random choice between 19-120
 var computerChoice = Math.floor(Math.random() * 101) + 19;
-    
+
+    //Global variables for each crystal.  Random selects number between 1-12
 var blue = Math.floor(Math.random() * 11) + 1;
 var red = Math.floor(Math.random() * 11) + 1;
 var purple = Math.floor(Math.random() * 11) + 1;
 var green = Math.floor(Math.random() * 11) + 1;
 
+    //Link variable to HTML Object
         $("#computerChoice").text(computerChoice);
         $("#winCounter").html(winCounter);
         $("#lossCounter").html(lossCounter);
         $("#playersTotalScore").text(counter);
 
+    //Reset Function
     function reset() {
 
-        computerChoice = Math.floor(Math.random() * 101) + 19;
-        
+        computerChoice = Math.floor(Math.random() * 101) + 19;        
         $("#computerChoice").text(computerChoice);
-
         blue = Math.floor(Math.random() * 11) + 1;
         red = Math.floor(Math.random() * 11) + 1;
         purple = Math.floor(Math.random() * 11) + 1;
         green = Math.floor(Math.random() * 11) + 1;
         counter = 0;
-    
         $("#playersTotalScore").text(counter);
 
     }
-
+    //Win Counter
     function win() {
 
         winCounter ++;
         $("#winCounter").text(winCounter);
-        alert("You win!");
+        alert("You Win!");
         reset();
-    }
 
+    }
+    //Loss Counter
     function lose() {
 
 	      lossCounter ++;
         $("#lossCounter").text(lossCounter);
-        alert("You lose!");
-	      reset();
+        alert("You Lose!");
+        reset();
+        
     }
-
+    //Crystals random values are added to PTS.  If value is greater than PTS - Win, if not Lose.
     $('#blue').on ('click', function() {
 
         counter = counter + blue;
         $('#playersTotalScore').text(counter); 
-
         if (counter == computerChoice) {
         win();
-        }
+      }
         else if (counter > computerChoice) {
-          lose();
-        }   
+        lose();
+      }   
     });  
 
     $('#red').on ('click', function() {
